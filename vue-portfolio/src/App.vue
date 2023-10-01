d2ada7ced96843fee4914d64af1036ed59c94a37
annisarisma <annisarisma117@gmail.com>
2023-10-01 23:13:44 +0700

    add vue project

---
+++ vue-portfolio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Halo</h1>
+  <Navbar/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: { Navbar }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-portfolio/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="modal">
+        <h1>Halo</h1>
+    </div>
+</template>
+
+<style lang="scss">
+.modal {
+    h1 {
+        color: brown;
+    }
+}
+</style>
